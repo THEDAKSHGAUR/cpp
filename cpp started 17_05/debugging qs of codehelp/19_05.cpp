50c4f0aa3a77bc9285da85650315c3af2f827e22
Daksh Gaur <gaur.daksh05@gmail.com>
2024-05-19 22:55:29 +0530

    4th commit

--- cpp started 17_05/debugging qs of codehelp/19_05.cpp
+++ cpp started 17_05/debugging qs of codehelp/19_05.cpp
@@ -1,16 +1,44 @@
+// Q1
+// #include<iostream>
+// using namespace std;
+
+// int main() {
+// 	char c;
+//     cin>>c;
+//     if('a'<=c && c<= 'z'){ //online an OR operator was used, changed it
+//         cout<<"0";
+//     }
+// 	else if('A'<=c && c<= 'Z'){
+//         cout<<"1";
+//     }
+//     else{
+//         cout<<"-1";
+//     }
+// }
+
+
+//Q2
+/*
+Pattern
+1
+23
+345
+4567
+*/
 #include<iostream>
 using namespace std;
-
-int main() {
-	char c;
-    cin>>c;
-    if('a'<=c && c<= 'z'){
-        cout<<"0";
-    }
-	else if('A'<=c && c<= 'Z'){
-        cout<<"1";
-    }
-    else{
-        cout<<"-1";
+int main(){
+    int n;
+    cin>>n;
+    int i=1;
+    while(i<n){
+        int j = i,count = 0;
+        while(count<i){
+            cout<<j;
+            j = j + 1;
+            count = count + 1;
+        }
+        cout<<"\n";
+        i = i + 1;
     }
 }
