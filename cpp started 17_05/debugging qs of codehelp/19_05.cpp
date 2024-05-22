51e6d2c49751bbade725834c25c5b9acaa995a55
Daksh Gaur <gaur.daksh05@gmail.com>
2024-05-22 13:13:25 +0530

    commit 1 of the day 22nd May

--- cpp started 17_05/debugging qs of codehelp/19_05.cpp
+++ cpp started 17_05/debugging qs of codehelp/19_05.cpp
@@ -25,20 +25,155 @@ Pattern
 345
 4567
 */
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cin>>n; //only cin was not asked.
+//     int i=1;
+//     while(i<n){
+//         int j = i,star = 0;
+//         while(star<i){
+//             cout<<j;
+//             j = j + 1;
+//             star = star + 1;
+//         }
+//         cout<<"\n";
+//         i = i + 1;
+//     }
+// }
+
+
+//Q3
+/*
+Pattern
+N = 4
+   *
+  ***
+ *****
+*******
+*/
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cin>>n;
+//     int i=1;
+//     while(i<n){ 
+//         int j = i,star = 2*n+1,gaps=(n -i-1),k=1;
+//         while(k<gaps){
+//             cout<<" ";
+//             k = k + 1;
+//         }
+//         int m = 1,ch = star - 2*gaps;
+//         while(m<ch){
+//             cout<<"*";
+//             m = m + 1;
+//         }
+//         k = 1;
+//         while(k<gaps){
+//             cout<<" ";
+//             k = k + 1;
+//         }
+//         cout<<"\n";
+//         i = i + 1;
+//     }
+// }
+//this is the alternate code for above code
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cin>>n;
+//     for(int i=0;i<n;i++){
+//         //for space
+//         for(int gap=0;gap<n-i-1;gap++){
+//             cout<<" "; 
+//         }
+//         for(int star=0;star<2*i+1;star++){ //instead of multiplying by 2 we can have an extra space in the below.
+//             cout<<"*";
+//         }
+//         cout<<endl;
+
+//     }
+   
+//     }
+
+
+//Q4
+/*
+Pattern
+N = 4
+1
+22
+333
+4444
+*/
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//   int i,j,n;
+//     cin>>n;
+//     for(i=1;i<=n;i++){
+//         for(j=1;j<i+1;j++){   //condition was incorrect.
+// 	        cout<<i;
+//         }
+//         cout<<endl;
+//     }
+// }
+
+
+//Q5
+/*
+Pattern
+N = 4
+1
+21
+321
+4321
+*/
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//   int i,j,n;
+//     cin>>n;
+//     for(i=1;i<n+1;i++){
+//         int p=i;
+//         for(j=1;j<i+1;j++){  //some incrementation and decrementation error was there.
+//           cout<<p;
+//           p--;
+//         }
+//         cout<<endl;
+        
+//     }
+// }
+
+
+//Q6
+/*
+Pattern
+N = 5
+E
+DE
+CDE
+BCDE
+ABCDE
+*/
 #include<iostream>
 using namespace std;
-int main(){
-    int n;
+
+int main() {
+    int i,j,n;;
     cin>>n;
-    int i=1;
-    while(i<n){
-        int j = i,count = 0;
-        while(count<i){
-            cout<<j;
-            j = j + 1;
-            count = count + 1;
+    for(i=0;i<=n;i++){
+        char p=65+n-i;
+        for(j=0;j<i;j++){
+            cout<<p;
+            p++;  
+            
         }
-        cout<<"\n";
-        i = i + 1;
+        cout<<endl;
     }
 }
