9c133bac97fb01896cce63c050044b8ab133d6e5
Daksh Gaur <gaur.daksh05@gmail.com>
2024-05-25 13:23:19 +0530

    first commit of 25th May

--- cpp started 17_05/prac codes/22_05.cpp
+++ cpp started 17_05/prac codes/22_05.cpp
@@ -33,26 +33,143 @@
 
 
 //HOLLOW TRIANGLE
+//MY CODE
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cin>>n;
+//         for(int i=1;i<=n;i++){
+//             for(int j=0;j<n-i;j++){
+//             cout<<" ";
+//         }
+//         for(int j=0;j<i;j++){
+//             if(j==0 || j==i-1){
+//                 cout<<"* ";
+//             }
+//             else{
+//                 cout<<" ";
+//             }
+//         }
+//         for(int j=0;j<n-i;j++){
+//             cout<<" ";
+//         }
+//         cout<<endl;
+// }
+// }
+
+//GPT CODE(MANIPULATED BY ME)
+// #include<iostream>
+// using namespace std;
+
+// int main() {
+//     int n;
+//     cin >> n;
+    
+//     for (int i = 1; i <= n; i++) {
+//         for (int j = 0; j < n - i; j++) {
+//             cout << " ";
+//         }
+        
+//         for (int j = 1; j <= i; j++) {
+//             if (j == 1 || j == i) {
+//                 cout << " *";
+//             } else {
+//                 cout << "  ";
+//             }
+//         }
+//         cout << endl;
+//     }
+    
+//     return 0;
+// }
+//to be done tomorrow(23rd May)
+//continued(1151hrs)
+
+
+//INVERTED HALLOW TIRANGLE
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cin>>n;
+//     for(int row=0;row<n;row++){
+//         for(int col=0;col<=row;col++){
+//             cout<<" ";
+//         }
+//         for(int col=0;col<n-row;col++){
+//             if(col==0 || col==n-row-1){
+//                 cout<<" *";
+//             }
+//             else{
+//                 cout<<"  ";
+//             }
+//         }
+//         for(int col;col<=row;col++){
+//             cout<<" ";
+//         }
+//         cout<<endl;
+//     }
+// }
+
+
+//LAST PATTERN
+//THE BUTTERFLY
 #include<iostream>
 using namespace std;
 int main(){
-    int n;
-    cin>>n;
-        for(int i=1;i<=n;i++){
-            for(int j=0;j<n-i;j++){
+    int row;
+    cin>>row;
+    for(int i=0;i<row;i++){
+        //triangle
+        for(int j=0;j<row-i;j++){
+            cout<<"*";
+        }
+        
+        //space
+         for(int j=0;j<i;j++){
             cout<<" ";
         }
-        for(int j=0;j<i;j++){
-            if(j==0 || j==i){
-                cout<<"* ";
-            }
-            else{
+       
+        //triangle
+        for(int j=0;j<1;j++){
+            for(int k=0;k<i;k++){
                 cout<<" ";
             }
+            for(int j=0;j<row-i;j++){
+                cout<<"*";
+            }
         }
-        for(int j=0;j<n-i;j++){
-            cout<<" ";
-        }
+        
+        
+        
         cout<<endl;
+    }
+    for(int i=0;i<row;i++){
+        //inverted triangle
+        for(int j=0;j<1;j++){
+            
+            for(int k=0;k<i+1;k++){
+                cout<<"*";
+            }
+            for(int k=0;k<row-0;k++){
+                cout<<" ";
+            }
+           
+        }
+
+        for(int j=0;j<1;j++){
+            for(int k=0;k<row-i-1;k++){
+                cout<<" ";
+            }
+
+            for(int k=0;k<i+1;k++){
+                cout<<"*";
+            }
+        }
+
+            cout<<endl;
+            
+    }
+
 }
-} //to be done tomorrow(23rd May)
