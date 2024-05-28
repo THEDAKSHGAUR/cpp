1ee6d9ecdd45ddd3a7a51ebd4746258ff9b1a6fb
Daksh Gaur <gaur.daksh05@gmail.com>
2024-05-28 07:21:03 +0530

    first commit of the morning

--- cpp started 17_05/prac codes/22_05.cpp
+++ cpp started 17_05/prac codes/22_05.cpp
@@ -114,62 +114,64 @@
 
 
 //LAST PATTERN
-//THE BUTTERFLY
-#include<iostream>
-using namespace std;
-int main(){
-    int row;
-    cin>>row;
-    for(int i=0;i<row;i++){
-        //triangle
-        for(int j=0;j<row-i;j++){
-            cout<<"*";
-        }
+//THE DIAMOND
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int row;
+//     cin>>row;
+//     for(int i=0;i<row;i++){
+//         //triangle
+//         for(int j=0;j<row-i;j++){
+//             cout<<"*";
+//         }
         
-        //space
-         for(int j=0;j<i;j++){
-            cout<<" ";
-        }
+//         //space
+//          for(int j=0;j<i;j++){
+//             cout<<" ";
+//         }
        
-        //triangle
-        for(int j=0;j<1;j++){
-            for(int k=0;k<i;k++){
-                cout<<" ";
-            }
-            for(int j=0;j<row-i;j++){
-                cout<<"*";
-            }
-        }
+//         //triangle
+//         for(int j=0;j<1;j++){
+//             for(int k=0;k<i;k++){
+//                 cout<<" ";
+//             }
+//             for(int j=0;j<row-i;j++){
+//                 cout<<"*";
+//             }
+//         }
         
         
         
-        cout<<endl;
-    }
-    for(int i=0;i<row;i++){
-        //inverted triangle
-        for(int j=0;j<1;j++){
+//         cout<<endl;
+//     }
+//     for(int i=0;i<row;i++){
+//         //inverted triangle
+//         for(int j=0;j<1;j++){
             
-            for(int k=0;k<i+1;k++){
-                cout<<"*";
-            }
-            for(int k=0;k<row-0;k++){
-                cout<<" ";
-            }
+//             for(int k=0;k<i+1;k++){
+//                 cout<<"*";
+//             }
+//             for(int k=0;k<row-0;k++){
+//                 cout<<" ";
+//             }
            
-        }
+//         }
 
-        for(int j=0;j<1;j++){
-            for(int k=0;k<row-i-1;k++){
-                cout<<" ";
-            }
+//         for(int j=0;j<1;j++){
+//             for(int k=0;k<row-i-1;k++){
+//                 cout<<" ";
+//             }
 
-            for(int k=0;k<i+1;k++){
-                cout<<"*";
-            }
-        }
+//             for(int k=0;k<i+1;k++){
+//                 cout<<"*";
+//             }
+//         }
 
-            cout<<endl;
+//             cout<<endl;
             
-    }
+//     }
 
-}
+// }
+
+//this code needs to be fixed.
