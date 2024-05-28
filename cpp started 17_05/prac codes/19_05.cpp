5d05f178f0ca0c60576808ac1d5e5ffcc1d642b0
Daksh Gaur <gaur.daksh05@gmail.com>
2024-05-28 16:46:37 +0530

    2nd commit of the day

--- cpp started 17_05/prac codes/19_05.cpp
+++ cpp started 17_05/prac codes/19_05.cpp
@@ -97,13 +97,13 @@
 // }
 
 //NUMERICAL INVERTED HALF PYRAMID
-#include<iostream>
-using namespace std;
-int main(){
-    for(int i=0;i<5;i++){
-        for(int j=0;j<5-i;j++){
-            cout<<j+1;
-        }
-        cout<<endl;
-    }
-}
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     for(int i=0;i<5;i++){
+//         for(int j=0;j<5-i;j++){
+//             cout<<j+1;
+//         }
+//         cout<<endl;
+//     }
+// }
---
+++ cpp started 17_05/prac codes/28_05.cpp
@@ -0,0 +1,33 @@
+//bitwise operator
+
+//NOT
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int a=5;
+//     cout<<~a<<endl;
+//     cout<<~(a)<<endl;
+//     cout<<(~a);
+// }
+
+//AND/OR;
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int a=5;
+//     int b=3;
+//     cout<<(a&b)<<endl;
+//     cout<<(a|b);
+// }
+
+//XOR 
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int a=5;
+//     int b=4;
+//     int c=5;
+//     cout<<(a^b)<<endl;
+//     cout<<(a^c);
+// }
+
