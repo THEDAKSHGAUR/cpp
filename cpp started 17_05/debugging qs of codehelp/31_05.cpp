b04f752b6820d1ae083d649bd384a4948b5ea959
Daksh Gaur <gaur.daksh05@gmail.com>
2024-05-31 11:38:41 +0530

    first commit of the day

---
+++ cpp started 17_05/debugging qs of codehelp/31_05.cpp
@@ -0,0 +1,13 @@
+//AREA OF TRIANGEL
+#include<iostream>
+using namespace std;
+
+int main() {
+	double width, height;
+	cin >> width >> height;
+	double area = 0.5*(width * height); //USE OF DOUBLE IS IMPORTENT
+    int integer=area;
+	cout << integer << endl;
+	return 0;
+}
+
