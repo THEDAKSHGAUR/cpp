69515500230a81973cc36f1ae8910ee36b537c3e
Daksh Gaur <gaur.daksh05@gmail.com>
2024-09-29 17:04:20 +0530

    LL videos completed

--- cpp started 05_17_24/prac codes/09.Linked List/09_29.cpp
+++ cpp started 05_17_24/prac codes/09.Linked List/09_29.cpp
@@ -6,38 +6,50 @@
 #include<limits.h>
 #include<numeric>
 using namespace std;
-class LinkList{
+class ListNode{
     public:
     int data;
-    LinkList *next;
-    LinkList(){
+    ListNode *next;
+    ListNode(){
         this->next=NULL;
     }
-    LinkList(int data){
+    ListNode(int data){
         this->data=data;
         this->next=NULL;
     }
 };
-void printLL(LinkList *head){
-    LinkList *temp=head;
+void printLL(ListNode *head){
+    ListNode *temp=head;
     while(temp!=NULL){
         cout<<temp->data<<" ";
         temp=temp->next;
     };
+    return;
 }
-LinkList* kReverse(LinkList *head,int k){
-    //base case
+int len(ListNode *head){
+        ListNode*temp=head;
+        int n=0;
+        while(temp!=NULL){
+            n++;
+            temp=temp->next;
+        }
+        return n;
+    }
+    ListNode* reverseKGroup(ListNode *head,int k){
     if(head==NULL){
         return head;
     }
     if(head->next==NULL){
         return head;
     }
-    //solve one case
     int p=0;
-    LinkList *prev=NULL;
-    LinkList *curr=curr;
-    LinkList *nextNode=NULL;
+    ListNode *prev=NULL;
+    ListNode *curr=head;
+    ListNode *nextNode=curr->next;
+    int L=len(head);
+    if(L<k){
+        return head;
+    }
     while(p<k){
         p++;
         nextNode=curr->next;
@@ -45,32 +57,44 @@ LinkList* kReverse(LinkList *head,int k){
         prev=curr;
         curr=nextNode;
     }
-    //recursive call;
-    LinkList* recursiveAns=NULL;
+    ListNode* recursiveAns=NULL;
     if(nextNode!=NULL){
-        recursiveAns==kReverse(nextNode,k);
-        nextNode->next=prev;
+        recursiveAns=reverseKGroup(nextNode,k);
+        head->next=recursiveAns;
     }
-    return recursiveAns;
+    return prev;
+}
+ListNode *removeDuplicates(ListNode *head){
+    if(head==NULL || head->next==NULL){
+        return head;
+    }
+    ListNode *temp=head;
+    while(temp!=NULL){
+        if(temp->next!=NULL && temp->data==temp->next->data){
+            ListNode *nextTemp=temp->next;
+            temp->next=nextTemp->next;
+        }
+        else{
+            temp=temp->next;
+        }
+    }
+    return head; 
 }
 int main(){
-    LinkList *numOne= new LinkList(1);
-    LinkList *numTwo= new LinkList(2);
-    LinkList *numThree= new LinkList(3);
-    LinkList *numFour= new LinkList(4);
-    LinkList *fast=numOne;
-    LinkList *slow=numOne;
-    LinkList *head=numOne;
+    ListNode *numOne= new ListNode(1);
+    ListNode *numTwo= new ListNode(2);
+    ListNode *numThree= new ListNode(2);
+    ListNode *numFour= new ListNode(2);
+    ListNode *fast=numOne;
+    ListNode *slow=numOne;
+    ListNode *head=numOne;
     numOne->next=numTwo;
     numTwo->next=numThree;
     numThree->next=numFour;
-    numFour->next=numOne;
-    //printing
-    printLL(head);
-    cout<<endl;
-    //kReversing
-    kReverse(head,2);
-    //print
+    numFour->next=NULL;
+    // //reverse for k
+    // reverseKGroup(head,2);
+    removeDuplicates(head);
     printLL(head);
     return 0;
 }
