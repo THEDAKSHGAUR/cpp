91a6b572f3d650e9aadff04ab108c7fd0ddc8e9d
Daksh Gaur <gaur.daksh05@gmail.com>
2024-12-31 22:22:56 +0530

    new year eve

---
+++ striver SWE sheet 191/Day1.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<vector>
+#include<string.h>
+#include<algorithm>
+#include<cmath>
+#include<limits.h>
+#include<numeric>
+#include<stack>
+#include<queue>
+#include<map>
+using namespace std;
+// //leet:73
+// void setZeroes(vector<vector<int>>& matrix) {
+//         vector<int>rVector;
+//         vector<int>cVector;
+//         int row=matrix.size();
+//         int col=matrix[0].size();
+//         for(int i=0;i<row;i++){
+//             for(int j=0;j<col;j++){
+//                 int element=matrix[i][j];
+//                 if(element==0){
+//                     rVector.push_back(i);
+//                     cVector.push_back(j);
+//                 }
+//             }
+//         }
+//         for(int i=0;i<rVector.size();i++){
+//             for(int j=0;j<col;j++){
+//                 matrix[rVector[i]][j]=0;
+//             }
+//         }
+//         for(int i=0;i<cVector.size();i++){
+//             for(int j=0;j<row;j++){
+//                 matrix[j][cVector[i]]=0;
+//             }
+//         }
+//         return;
+//     }
+int main(){
+    return 0;
+}
